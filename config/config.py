a2bd96b660636abdee5d37e60f2d3932208021b8
gabriel <gmaher2@hotmail.com>
2018-05-17 14:51:30 -0700

    folder structure

---
+++ config/config.py
@@ -0,0 +1,14 @@
+import os
+
+#######################################################
+# Directories
+#######################################################
+configDir = os.path.dirname(os.path.realpath(__file__))
+
+srcDir = os.path.abspath(configDir+'/..')
+
+dataDir = srcDir+'/data'
+
+#######################################################
+# Simulation Inputs
+#######################################################
---
+++ main.py
@@ -0,0 +1,3 @@
+import argparse
+
+from config import config
