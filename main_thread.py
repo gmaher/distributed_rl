3ea2bb8f096c32909ea08d94be4b574100df561e
gabriel <gmaher2@hotmail.com>
2018-05-18 17:31:27 -0700

    actor thread

---
+++ main_thread.py
@@ -0,0 +1,38 @@
+import argparse
+
+from config import config
+from src.util import read_json
+from src.environment import get_environment
+from src.exploration import EpsGreedy
+from src.agent import TabularQFunction
+from src.replay_buffer import ReplayBuffer
+from src.actor import ActorThread
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-env', type=str)
+
+args = parser.parse_args()
+
+#######################################
+# Read inputs
+#######################################
+env_input = read_json(args.env)
+
+#######################################
+# Set up simulation
+#######################################
+env = get_environment(env_input)
+
+explorer = EpsGreedy(num_actions=env_input['NUM_ACTIONS'],
+    eps=config.EPS_START, eps_min=config.EPS_MIN,
+        decay=config.DECAY)
+
+agent = TabularQFunction(state_size=env_input['STATE_SIZE'][0],
+    num_actions=env_input['NUM_ACTIONS'],
+    mu_init=config.Q_INIT, std_init=config.Q_STD)
+
+
+replay = ReplayBuffer()
+
+actor  = ActorThread(agent, env, explorer, replay, config)
+actor.start()
---
+++ src/actor.py
@@ -0,0 +1,63 @@
+import numpy as np
+import time
+import threading
+import random
+import logging
+
+logging.basicConfig(level=logging.DEBUG,
+    format='(%(threadName)-9s) %(message)s',)
+
+class ActorThread(threading.Thread):
+    def __init__(self, agent, env, explorer, replay_buffer, config, group=None, target=None, name="actor", args=(), kwargs=None, verbose=None):
+        super(ActorThread,self).__init__()
+
+        self.group  = group
+        self.target = target
+        self.name   = name
+
+        self.agent         = agent
+        self.env           = env
+        self.explorer      = explorer
+        self.replay_buffer = replay_buffer
+        self.config        = config
+        #self.setDaemon(True)
+
+    def run(self):
+        count = 0
+        while True:
+            count+=1
+
+            s = self.env.reset()
+
+            for t in range(self.config.NUM_STEPS):
+
+                if (count%self.config.RENDER_FREQUENCY == 0)\
+                and self.config.RENDER:
+                    self.env.render()
+
+                a = self.agent.act(s)
+
+                a = self.explorer.explore(a)
+
+                ss,r,done,info = self.env.step(a)
+
+                self.replay_buffer.add((s,a,r,ss,done))
+
+                if len(self.replay_buffer.tuples) > 1:
+                    tup = self.replay_buffer.get(batch_size=1)
+
+                    self.agent.update(tup[0][0], tup[1][0], tup[2][0],
+                        tup[3][0], self.config.LEARNING_RATE)
+
+                s = ss
+
+                if done:
+                    break
+
+            logging.debug("episode {}: final state {}, reward {}".format(count, s, r))
+
+            self.explorer.update()
+
+            time.sleep(random.random())
+
+        return
